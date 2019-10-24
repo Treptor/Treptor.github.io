ecb5ab5483e006fe2773d2a8c2ce88b3a67ae6e4
Treptor <33964796+Treptor@users.noreply.github.com>
2019-10-24 18:22:19 +0300

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,75 +1,29 @@
-var hoursContainer = document.querySelector('.hours')
-var minutesContainer = document.querySelector('.minutes')
-var secondsContainer = document.querySelector('.seconds')
-var tickElements = Array.from(document.querySelectorAll('.tick'))
-
-var last = new Date(0)
-last.setUTCHours(-1)
-
-var tickState = true
-
-function updateTime () {
-  var now = new Date
-  
-  var lastHours = last.getHours().toString()
-  var nowHours = now.getHours().toString()
-  if (lastHours !== nowHours) {
-    updateContainer(hoursContainer, nowHours)
-  }
-  
-  var lastMinutes = last.getMinutes().toString()
-  var nowMinutes = now.getMinutes().toString()
-  if (lastMinutes !== nowMinutes) {
-    updateContainer(minutesContainer, nowMinutes)
-  }
-  
-  var lastSeconds = last.getSeconds().toString()
-  var nowSeconds = now.getSeconds().toString()
-  if (lastSeconds !== nowSeconds) {
-    //tick()
-    updateContainer(secondsContainer, nowSeconds)
-  }
-  
-  last = now
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
 }
 
-function tick () {
-  tickElements.forEach(t => t.classList.toggle('tick-hidden'))
-}
-
-function updateContainer (container, newTime) {
-  var time = newTime.split('')
-  
-  if (time.length === 1) {
-    time.unshift('0')
-  }
-  
-  
-  var first = container.firstElementChild
-  if (first.lastElementChild.textContent !== time[0]) {
-    updateNumber(first, time[0])
-  }
-  
-  var last = container.lastElementChild
-  if (last.lastElementChild.textContent !== time[1]) {
-    updateNumber(last, time[1])
-  }
-}
-
-function updateNumber (element, number) {
-  //element.lastElementChild.textContent = number
-  var second = element.lastElementChild.cloneNode(true)
-  second.textContent = number
-  
-  element.appendChild(second)
-  element.classList.add('move')
-
-  setTimeout(function () {
-    element.classList.remove('move')
-  }, 990)
-  setTimeout(function () {
-    element.removeChild(element.firstElementChild)
-  }, 990)
-}
-
-setInterval(updateTime, 100)
+showTime();
